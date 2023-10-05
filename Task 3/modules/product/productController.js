96dde3d0e6d9ea7a970cacefcb61c0f72959c182
Roshan <roshanpokhrel013@gmail.com>
2023-10-06 01:37:48 +0545

    CRUD products

--- Task 3/modules/product/productController.js
+++ Task 3/modules/product/productController.js
@@ -24,5 +24,19 @@ productController.deleteProduct = async (req,res) => {
   await(Product.findByIdAndDelete(req.params.id))
   res.status(200).send("Product Deleted Successfully")
 }
+
+
+productController.searchProduct = async (req,res) =>{
+  //const filter = req.params.id;
+  const query = req.params.productName;
+  const product = await Product.find({productName : {$regex: new RegExp(query, 'i')}})
+  if (!product){
+    res.status(404).send("Product does not exist")
+  }
+  else{
+    res.send(product)
+  }
+}
+
 console.log(productController);
 module.exports = productController;
--- Task 3/routes/products.js
+++ Task 3/routes/products.js
@@ -2,7 +2,8 @@ const productController = require("../modules/product/productController");
 
 const router = require("express").Router();
 
-router.get("/", productController.displayProduct)
+router.get("/", productController.displayProduct);
+router.get("/:productName", productController.searchProduct);
 router.post("/", productController.addProduct);
 
 router.delete("/:id", productController.deleteProduct);
