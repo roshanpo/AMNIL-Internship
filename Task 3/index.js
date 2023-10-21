65d8584570114112c9f74eec00301cc90a3dc62d
Roshan Pokhrel <roshanpokhrel013@gmail.com>
2023-10-21 21:22:18 +0545

    Added Authentication with google account

--- Task 3/index.js
+++ Task 3/index.js
@@ -1,15 +1,58 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+//import { dotenv } from "config";
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const Order = require('./models/Order');
 const Product = require('./models/Product');
+const path = require('path'); 
+//const ejsLint = require('ejs-lint');
+//const firebaseConfig = require('./config/firebase');
+//import {firebaseConfig} from './config/firebase.mjs'
+
+
+//swagger
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Ecommerce API',
+    version: '1.0.0',
+    description:'This is a REST API application made with Express.',
+  },
+  
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 
 
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const indexRouter = require("./routes/index");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
 
 app.use(express.json());
 
--- Task 3/modules/user/userController.js
+++ Task 3/modules/user/userController.js
@@ -2,8 +2,43 @@ const Users = require("../../models/User");
 const bcrypt = require("bcrypt")
 //const basicAuth = require('../../middleware/basicAuth')
 require('dotenv').config();
+const { initializeApp } = require('firebase-admin/app');
+const admin = require('firebase-admin');
+const serviceAccount = require('../../serviceAccountKey.json');
 
 const userController = {};
+
+
+
+userController.googleLogin = async (req, res) => {
+
+  if (!admin.apps.length) {
+    const firebaseAdmin = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
+
+  res.render('login')
+  const loginData = req.body;
+  const idToken = String(loginData.idToken);
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      console.log('User Verified');
+      console.log('User ID:', decodedToken.uid);
+      console.log('Email:', decodedToken.email);
+      console.log('Name: ', decodedToken.name);
+
+
+      // The token is valid. You can trust this user.
+    })
+    .catch((error) => {
+      console.error('Token verification error:', error);
+    });
+
+}
 
 userController.loginUser = async (req, res) => {
   const { username, password } = req.body;
@@ -11,20 +46,20 @@ userController.loginUser = async (req, res) => {
   const user = await Users.findOne({ username });
 
   if (!user) {
-      return res.status(404).send('User not found');
+    return res.status(404).send('User not found');
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
-      return res.status(400).send('Invalid password');
+    return res.status(400).send('Invalid password');
   }
 
   const payload = {
-      user: {
-          id: user._id,
-          username: user.username
-      }
+    user: {
+      id: user._id,
+      username: user.username
+    }
   }
 
   const authToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1d' });
@@ -43,46 +78,54 @@ userController.createUser = async (req, res) => {
   const user = {
     name: req.body.name,
     email: req.body.email,
-    username : req.body.username,
-    password : req.body.password
+    username: req.body.username,
+    password: req.body.password
 
-}
-const newUser = await Users.create(user);
-const payload = {
-  user: {
+  }
+ // const newUser = await Users.create(user);
+  try {
+    const newUser = await Users.create(user);
+    res.status(201);
+    
+  } catch (error) {
+    res.status(400)
+  }
+  
+  const payload = {
+    user: {
       id: newUser._id,
       username: newUser.username
+    }
   }
-}
 
-const authToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1d' });
+  const authToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1d' });
 
-newUser.image = getImageUrl(req, newUser.image);
-res.status(201).send({ newUser, authToken });
+  newUser.image = getImageUrl(req, newUser.image);
+  res.status(201).send({ newUser, authToken });
 
-newUser.save();
+  newUser.save();
 
   res.send(newUser);
 };
 
-userController.updateUser = async (req,res) => {
+userController.updateUser = async (req, res) => {
   const user = await Users.findOne(req.params.name);
-  if (!user){
+  if (!user) {
     this.createUser;
   }
   const filter = req.params.id;
   const updateUser = {
-    name : req.body.name,
-    email : req.body.email,
+    name: req.body.name,
+    email: req.body.email,
   }
-  const updatedUser = await Users.findByIdAndUpdate(filter, updateUser, {new : true});
+  const updatedUser = await Users.findByIdAndUpdate(filter, updateUser, { new: true });
   res.send(updatedUser)
 }
 
 userController.deleteUser = async (req, res) => {
   const user = await Users.findById(req.params.id);
   if (!user) {
-      return res.send('User not found');
+    return res.send('User not found');
   }
 
   await Users.findByIdAndDelete(req.params.id);
